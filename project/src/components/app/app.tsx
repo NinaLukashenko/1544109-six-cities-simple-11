0cd4839e555d6d2da89c721db6e2ffaf38cce047
NinaLukashenko <nina.lucashenco@gmail.com>
2022-11-29 16:15:03 +0100

    remove not used method

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -8,7 +8,7 @@ import Login from '../../pages/login/login';
 import Main from '../../pages/main/main';
 import NotFound from '../../pages/not-found/not-found';
 import Room from '../../pages/room/room';
-import { getIsLoading, getOffers } from '../../store/offers-data/selectors';
+import { getIsLoading } from '../../store/offers-data/selectors';
 import { getAuthorizationStatus } from '../../store/user-process/selectors';
 import { Cities } from '../../types/city';
 import HistoryRouter from '../history-route/history-route';
